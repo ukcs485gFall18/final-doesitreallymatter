cd31d560c94828f6b99b65fe0d58156175e78ada
Kenton Carrier <kncarrier28@gmail.com>
2018-12-01 14:20:27 -0500

    Added more profile attributes.

--- RestaurantRoyale/ProfileViewController.swift
+++ RestaurantRoyale/ProfileViewController.swift
@@ -11,13 +11,25 @@ import Firebase
 
 class ProfileViewController: UIViewController {
 
-  @IBOutlet weak var profilePic: UIImageView!
-  @IBOutlet weak var usernameDisplay: UILabel!
+    @IBOutlet weak var favFoodDisplay: UILabel!
+    @IBOutlet weak var usernameDisplay: UILabel!
+    @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var nameDisplay: UILabel!
   
   override func viewDidLoad() {
         super.viewDidLoad()
 
     let user = Auth.auth().currentUser
+    let userDocRef = db.collection("userData").document((user?.uid)!)
+    
+    userDocRef.getDocument() { (document, error) in
+      if let document = document {
+        self.favFoodDisplay.text = document.get("favFood") as? String
+        self.nameDisplay.text = document.get("fullName") as? String
+      } else {
+        print("Document does not exist")
+      }
+    }
     
     usernameDisplay.text = user?.displayName
     profilePic.load(url: (user?.photoURL)!)
