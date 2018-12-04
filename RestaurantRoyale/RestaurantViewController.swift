b4a9d94b30b8d645eb600a4d8bfcce2e808b57ae
jmench <jmench97@gmail.com>
2018-12-03 23:37:27 -0500

    - GUI changes

---
+++ RestaurantRoyale/RestaurantViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantViewController.swift
+//  Restaurant Royale
+//
+//  Created by Jordan Menchen on 12/3/18.
+//  Copyright © 2018 Ken Toh. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+  
+  
+}
